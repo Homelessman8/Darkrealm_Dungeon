13c8fb19cccab98bb43d6522f08f65abfe5b267b
KovalenVel <kovalen1499@gmail.com>
2023-09-05 04:47:04 +1000

    Added Adventurer, sword, coins
    Organized folders and assets
    Merged Mansoo's files
    
    Co-Authored-By: Homelessman8 <123073449+Homelessman8@users.noreply.github.com>

---
+++ World Interaction01/Assets/Adventurer/ItemCollector.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int coins = 0; //Count the coins
+    public int weapon = 0; //Count the weapons
+    //weaponEquipped checks if the player is armed
+    public bool weaponEquipped;
+    Animator animator;
+
+    [SerializeField] private Text coinText;
+
+    void Awake()
+    {
+        //rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    //when we collide with the item
+    private void OnTriggerEnter2D(Collider2D collision)//because items are set to istrigger
+    {
+        if (collision.gameObject.CompareTag("Coin"))//if we are colliding with a coin
+        {
+            //make the item disappear from the game
+            Destroy(collision.gameObject);
+            coins++; //increment count after collecting coin
+            coinText.text = "Coins: " + coins;
+            //Debug.Log("Coins: " + coins);
+        }
+
+        if (collision.gameObject.CompareTag("Weapon"))//if we are colliding with a weapon
+        {
+            //make the item disappear from the game
+            Destroy(collision.gameObject);
+            weapon++; //increment count after collecting weapon
+            //coinText.text = "Coins: " + coins;
+            Debug.Log("Weapon obtained: " + weapon);
+            if (weapon != 0)
+            {
+                animator.SetBool("weaponEquipped", weaponEquipped);
+            }
+        }
+    }
+}
