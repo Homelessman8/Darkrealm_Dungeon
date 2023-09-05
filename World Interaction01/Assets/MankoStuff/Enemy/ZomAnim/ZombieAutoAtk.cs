6f1c85846eaabc9138f673da0a8f4453689132de
Homelessman8 <sookram1208@gmail.com>
2023-09-05 16:04:19 +1000

    ZomAnimScript Updated1
    animation of zom works now but needs polish with AI path

--- World Interaction01/Assets/MankoStuff/Enemy/ZomAnim/ZombieAutoAtk.cs
+++ World Interaction01/Assets/MankoStuff/Enemy/ZomAnim/ZombieAutoAtk.cs
@@ -7,8 +7,10 @@ public class ZombieAutoAtk : MonoBehaviour
     public GameObject player;         // Reference to the player GameObject.
     public float speed;               // Speed at which the zombie moves.
     public float distanceBetween;     // Minimum distance to start moving towards the player.
+    public Animator anim;
 
-    private Animator anim;
+
+    private Vector2 defaultPos;
     private float distance;           // Distance between the zombie and the player.
 
     // Start is called before the first frame update
@@ -16,11 +18,14 @@ public class ZombieAutoAtk : MonoBehaviour
     {
         // This method is called once when the script starts.
         // You can initialize any variables or setup here if needed.
+        defaultPos = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("this id x and y pos:" + defaultPos.x + defaultPos.y);
         // Calculate the distance between the zombie and the player.
         distance = Vector2.Distance(transform.position, player.transform.position);
 
@@ -40,6 +45,11 @@ public class ZombieAutoAtk : MonoBehaviour
             // Rotate the zombie to face the player based on the calculated angle.
             //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
+        else if(distance>distanceBetween)
+        {
+            
+            anim.SetBool("IdleFront", true);
+        }
 
 
         //Animation for Zombie Movement
@@ -47,34 +57,45 @@ public class ZombieAutoAtk : MonoBehaviour
         if(direction.x > 0f)
         {
             anim.SetBool("MoveRight", true);
-            //anim.SetBool("MoveLeft", false);
-            //anim.SetBool("MoveFront", false);
-            //anim.SetBool("MoveBack", false);
-            //anim.SetBool("IdleFront", false);
+            anim.SetBool("MoveLeft", false);
+            anim.SetBool("MoveFront", false);
+            anim.SetBool("MoveBack", false);
+            anim.SetBool("IdleFront", false);
+            Debug.Log("This is the direction X" + direction.x);
+            Debug.Log("MoveRight" + anim.GetBool("MoveRight"));
+            Debug.Log("MoveLeft" + anim.GetBool("MoveLeft"));
+            Debug.Log("MoveFront" + anim.GetBool("MoveFront"));
+            Debug.Log("MoveBack" + anim.GetBool("MoveBack"));
         }
         else if (direction.x < 0f)
         {
-            //anim.SetBool("MoveRight", false);
+            anim.SetBool("MoveRight", false);
             anim.SetBool("MoveLeft", true);
-            //anim.SetBool("MoveFront", false);
-            //anim.SetBool("MoveBack", false);
-            //anim.SetBool("IdleFront", false);
+            anim.SetBool("MoveFront", false);
+            anim.SetBool("MoveBack", false);
+            anim.SetBool("IdleFront", false);
         }
         else if (direction.y < 0f)
         {
-            //anim.SetBool("MoveRight", false);
-            //anim.SetBool("MoveLeft", false);
+            anim.SetBool("MoveRight", false);
+            anim.SetBool("MoveLeft", false);
             anim.SetBool("MoveFront", true);
-            //anim.SetBool("MoveBack", false);
-            //anim.SetBool("IdleFront", false);
+            anim.SetBool("MoveBack", false);
+            anim.SetBool("IdleFront", false);
+            Debug.Log("This is the direction Y" + direction.y);
+            Debug.Log("MoveRight" + anim.GetBool("MoveRight"));
+            Debug.Log("MoveLeft" + anim.GetBool("MoveLeft"));
+            Debug.Log("MoveFront" + anim.GetBool("MoveFront"));
+            Debug.Log("MoveBack" + anim.GetBool("MoveBack"));
         }
+    
         else if (direction.y > 0f)
         {
-            //anim.SetBool("MoveRight", false);
-            //anim.SetBool("MoveLeft", false);
-            //anim.SetBool("MoveFront", false);
+            anim.SetBool("MoveRight", false);
+            anim.SetBool("MoveLeft", false);
+            anim.SetBool("MoveFront", false);
             anim.SetBool("MoveBack", true);
-            //anim.SetBool("IdleFront", false);
+            anim.SetBool("IdleFront", false);
         }
         else
         {
