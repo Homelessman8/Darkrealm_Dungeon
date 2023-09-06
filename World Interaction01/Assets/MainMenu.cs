2342e8f0209ca50d886ff4f910f105c3f4fb0754
Homelessman8 <sookram1208@gmail.com>
2023-09-07 01:47:15 +1000

    MenuStuff6 + PauseMenu2
    linked all buttons to have functions on click etc etc

--- World Interaction01/Assets/MainMenu.cs
+++ World Interaction01/Assets/MainMenu.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
 
     // Update is called once per frame
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game..");
+        Application.Quit();
     }
 }
